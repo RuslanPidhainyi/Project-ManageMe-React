300ff2f524f0e8e32086712941746ee85e103c18
RuslanPidhainyi <ruslanpidgauny@gmai.com>
2024-04-12 12:52:49 +0200

    Fake user profile v 4.0.4

---
+++ src/Data/FakeDataUser/FakeDataUser.ts
@@ -0,0 +1,24 @@
+// import { ProjectModelType } from "../../Types/ProjectModel.type/ProjectModel.type";
+
+// export const fakeDbProjects: ProjectModelType[] = [
+//   { id: 1
+//     , name: "MenegMe", desc: "LoremLoremLoremLoremLoremLoremLoremLorem" },
+//   {
+//     id: 2,
+//     name: "AppCinema-ASP-MVC",
+//     desc: "Cinema App. Internet page for buying tickets to the film in various cinemas. Made this Framework ASP.Net on architecture MVC",
+//   },
+//   {
+//     id: 3,
+//     name: "Social-Media-App-React",
+//     desc: "Social Media App where users can post photos and posts and view other users photos and posts. Written in React framework using TypeScript.",
+//   },
+// ];
+
+import { UserType } from "../../Types/User.type/User.type";
+
+export const fakeDbUser: UserType = {
+  id: "d-2022-h-2024",
+  name: "David",
+  surname: "Chemik",
+};
